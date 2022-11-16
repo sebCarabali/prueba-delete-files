f354ada77d5a1148b1f141150db93650578a52cd
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2022-11-16 17:28:36 -0500

    Se crea funcion factorial

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hola mundo')
+def factorial(n: int) -> int:
+    memo = {}
+    def fact(x: int, m: dict):
+        if m.get(x) != None:
+            return m.get(x)
+        elif x == 0:
+            return 1
+        else:
+            m[x] = fact(x-1, m) * x
+            return m[x]
+    return fact(n, memo)
+
+print(factorial(5))
